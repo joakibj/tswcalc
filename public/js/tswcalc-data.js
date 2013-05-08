ea32cab34871b145e8a79d478208887e5c8eda3f
joakibj <joakibj@gmail.com>
2013-05-09 00:44:13 +0200

    Added data for custom gear and primary stats

--- public/js/tswcalc-data.js
+++ public/js/tswcalc-data.js
@@ -26,3 +26,132 @@ template_data = {
         name: 'Occult Talisman'
     }]
 }
+
+custom_gear_data = {
+    "head": {
+        heal_dps: {
+            ql0: {
+                rating: 559
+            },
+            ql1: {
+                rating: 596
+            },
+            ql2: {
+                rating: 636
+            },
+            ql3: {
+                rating: 682
+            },
+            ql4: {
+                rating: 735
+            },
+            ql5: {
+                rating: 788
+            }
+        },
+        tank: {
+            ql0: {
+                hitpoints: 2100
+            },
+            ql1: {
+                hitpoints: 2194
+            },
+            ql2: {
+                hitpoints: 2288
+            },
+            ql3: {
+                hitpoints: 2382
+            },
+            ql4: {
+                hitpoints: 2476
+            },
+            ql5: {
+                hitpoints: 2570
+            }
+        }
+    },
+    "major": {
+        heal_dps: {
+            ql0: {
+                rating: 505
+            },
+            ql1: {
+                rating: 538
+            },
+            ql2: {
+                rating: 575
+            },
+            ql3: {
+                rating: 616
+            },
+            ql4: {
+                rating: 664
+            },
+            ql5: {
+                rating: 712
+            }
+        },
+        tank: {
+            ql0: {
+                hitpoints: 1897
+            },
+            ql1: {
+                hitpoints: 1982
+            },
+            ql2: {
+                hitpoints: 2067
+            },
+            ql3: {
+                hitpoints: 2152
+            },
+            ql4: {
+                hitpoints: 2237
+            },
+            ql5: {
+                hitpoints: 2322
+            }
+        }
+    },
+    "minor": {
+        heal_dps: {
+            ql0: {
+                rating: 325
+            },
+            ql1: {
+                rating: 346
+            },
+            ql2: {
+                rating: 369
+            },
+            ql3: {
+                rating: 396
+            },
+            ql4: {
+                rating: 427
+            },
+            ql5: {
+                rating: 458
+            }
+        },
+        tank: {
+            ql0: {
+                hitpoints: 1220
+            },
+            ql1: {
+                hitpoints: 1274
+            },
+            ql2: {
+                hitpoints: 1329
+            },
+            ql3: {
+                hitpoints: 1383
+            },
+            ql4: {
+                hitpoints: 1438
+            },
+            ql5: {
+                hitpoints: 1492
+            }
+        }
+    }
+}
