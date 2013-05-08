3ce84f7f7ce3bf56f3cefb06b8eeed6f601b2200
joakibj <joakibj@gmail.com>
2013-05-09 01:32:38 +0200

    Added primary stat updater

--- public/js/tswcalc.js
+++ public/js/tswcalc.js
@@ -17,10 +17,32 @@ function render_container(data) {
 }
 
 function start_buttonHandlers() {
-    for(var i = 0; i < template_data.slots.length; i++) {
+    for (var i = 0; i < template_data.slots.length; i++) {
         var handler = new ButtonHandler(template_data.slots[i].id_prefix);
         handler.initiate();
     }
+}
+
+function updatePrimaryStats() {
+    sumHitPoints = 0;
+    sumAttackRating = 0;
+    sumHealRating = 0;
+    for (var i = 0; i < template_data.slots.length; i++) {
+        var role = $('#' + template_data.slots[i].id_prefix + '-role option:selected');
+        var ql = $('#' + template_data.slots[i].id_prefix + '-ql > button.btn.active')[0].innerHTML;
+        selectedValue = role.attr('value');
+        console.log(ql);
+        if (selectedValue == 'dps') {
+            sumAttackRating += custom_gear_data[template_data.slots[i].group].heal_dps['ql' + (ql)].rating;
+        } else if (selectedValue == 'healer') {
+            sumHealRating += custom_gear_data[template_data.slots[i].group].heal_dps['ql' + (ql)].rating;
+        } else if (selectedValue == 'tank') {
+            sumHitPoints += custom_gear_data[template_data.slots[i].group].tank['ql' + (ql)].hitpoints;
+        }
+    }
+    $('#stat-hitpoints').text(sumHitPoints);
+    $('#stat-attack-rating').text(sumAttackRating);
+    $('#stat-heal-rating').text(sumHealRating);
 }
 
 function ButtonHandler(slotId) {
@@ -34,38 +56,42 @@ function ButtonHandler(slotId) {
     };
 
     this.addListenersToSlotQlButtons = function(id_prefix) {
-        buttons = $('#'+id_prefix+'-ql > button.btn').on('click', function(event) {
-            self.onlyActiveButton('#'+this.id);
+        self.onlyActiveButton('#' + slotId + '-ql-btn0');
+        buttons = $('#' + id_prefix + '-ql > button.btn').on('click', function(event) {
+            self.onlyActiveButton('#' + this.id);
+            updatePrimaryStats();
         });
     };
 
     this.addListenersToGlyphQlButtons = function(id_prefix, glyph) {
-        buttons = $('#'+id_prefix+'-'+glyph+'-ql > button.btn').on('click', function(event) {
-            self.onlyActiveButton('#'+this.id);
+        self.onlyActiveButton('#' + slotId + '-primary-glyph-ql-btn0');
+        self.onlyActiveButton('#' + slotId + '-secondary-glyph-ql-btn0');
+        buttons = $('#' + id_prefix + '-' + glyph + '-ql > button.btn').on('click', function(event) {
+            self.onlyActiveButton('#' + this.id);
             self.balanceGlyphQl(this, glyph);
         });
     };
 
-    //TODO: Refactor this!
+    //TODO: Refactor this! Also: Bugs.
     this.balanceGlyphQl = function(button, glyph) {
-        if(glyph == 'primary-glyph') {
+        if (glyph == 'primary-glyph') {
             primaryGlyphQl = parseInt(button.innerHTML);
-            activeSecondaryGlyph = $('#'+slotId+'-secondary-glyph-ql > button.btn.active');
-            if($(activeSecondaryGlyph).length > 0) {
+            activeSecondaryGlyph = $('#' + slotId + '-secondary-glyph-ql > button.btn.active');
+            if ($(activeSecondaryGlyph).length > 0) {
                 secondaryGlyphQl = parseInt(activeSecondaryGlyph[0].innerHTML);
-                if(primaryGlyphQl + secondaryGlyphQl > 5) {
-                    oneLowerQlId = activeSecondaryGlyph[0].id.substring(0, activeSecondaryGlyph[0].id.length-1) + (secondaryGlyphQl-(primaryGlyphQl + secondaryGlyphQl - 5));
-                    self.onlyActiveButton('#'+oneLowerQlId);
+                if (primaryGlyphQl + secondaryGlyphQl > 5) {
+                    oneLowerQlId = activeSecondaryGlyph[0].id.substring(0, activeSecondaryGlyph[0].id.length - 1) + (secondaryGlyphQl - (primaryGlyphQl + secondaryGlyphQl - 5));
+                    self.onlyActiveButton('#' + oneLowerQlId);
                 }
             }
         } else {
             secondaryGlyphQl = parseInt(button.innerHTML);
-            activePrimaryGlyph = $('#'+slotId+'-primary-glyph-ql > button.btn.active');
-            if($(activePrimaryGlyph).length > 0) {
+            activePrimaryGlyph = $('#' + slotId + '-primary-glyph-ql > button.btn.active');
+            if ($(activePrimaryGlyph).length > 0) {
                 primaryGlyphQl = parseInt(activePrimaryGlyph[0].innerHTML);
-                if(primaryGlyphQl + secondaryGlyphQl > 5) {
-                    oneLowerQlId = activePrimaryGlyph[0].id.substring(0, activePrimaryGlyph[0].id.length-1) + + (secondaryGlyphQl-(primaryGlyphQl + secondaryGlyphQl - 5));
-                    self.onlyActiveButton('#'+oneLowerQlId);
+                if (primaryGlyphQl + secondaryGlyphQl > 5) {
+                    oneLowerQlId = activePrimaryGlyph[0].id.substring(0, activePrimaryGlyph[0].id.length - 1) + +(secondaryGlyphQl - (primaryGlyphQl + secondaryGlyphQl - 5));
+                    self.onlyActiveButton('#' + oneLowerQlId);
                 }
             }
         }
@@ -76,9 +102,5 @@ function ButtonHandler(slotId) {
         $(id).siblings().removeClass('btn-success');
         $(id).addClass('active');
         $(id).addClass('btn-success');
-    };
-
-    this.findButtonIndex = function(id) {
-        return id.charAt(id.length-1);
     };
 }
