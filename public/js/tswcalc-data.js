0721c9268ae87436dc416064474fb69258d8de30
Joakim Bjornstad <joakibj@gmail.com>
2013-05-08 14:57:15 +0200

    Added data and slot template

---
+++ public/js/tswcalc-data.js
@@ -0,0 +1,27 @@
+data = {
+        slots: [{
+            id_prefix: 'weapon',
+            name: 'Weapon'
+        }, {
+            id_prefix: 'head',
+            name: 'Head Talisman'
+        }, {
+            id_prefix: 'ring',
+            name: 'Ring Talisman'
+        }, {
+            id_prefix: 'neck',
+            name: 'Neck Talisman'
+        }, {
+            id_prefix: 'wrist',
+            name: 'Wrist Talisman'
+        }, {
+            id_prefix: 'luck',
+            name: 'Luck Talisman'
+        }, {
+            id_prefix: 'waist',
+            name: 'Waist Talisman'
+        }, {
+            id_prefix: 'occult',
+            name: 'Occult Talisman'
+        }]
+    }
--- public/js/tswcalc.js
+++ public/js/tswcalc.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-    data = {
-        slots: [{id_prefix: 'weapon', name: 'Weapon'}, {id_prefix: 'head', name: 'Head Talisman'}]
+    render_container(data);
+
+    function render_container(data) {
+        dust.render("container", data,
+
+        function(err, out) {
+            if (err) {
+                console.log(err);
+            }
+            $('.container').html(out);
+        });
     }
-
-    dust.render("container", data,
-
-    function(err, out) {
-        if (err) {
-            console.log(err);
-        }
-        $('.container').html(out);
-    });
 });
