2e910dba6df9a48e76fc34ae590a0605c356431f
joakibj <joakibj@gmail.com>
2013-05-09 01:08:39 +0200

    Changed ql in gear to an index. Added group value to template_data

--- public/js/tswcalc-data.js
+++ public/js/tswcalc-data.js
@@ -2,154 +2,162 @@ template_data = {
     slots: [{
         id_prefix: 'weapon',
         name: 'Weapon',
-        is_weapon: true
+        is_weapon: true,
+        group: 'weapon'
     }, {
         id_prefix: 'head',
-        name: 'Head Talisman'
+        name: 'Head Talisman',
+        group: 'head'
     }, {
         id_prefix: 'ring',
-        name: 'Ring Talisman'
+        name: 'Ring Talisman',
+        group: 'major'
     }, {
         id_prefix: 'neck',
-        name: 'Neck Talisman'
+        name: 'Neck Talisman',
+        group: 'major'
     }, {
         id_prefix: 'wrist',
-        name: 'Wrist Talisman'
+        name: 'Wrist Talisman',
+        group: 'major'
     }, {
         id_prefix: 'luck',
-        name: 'Luck Talisman'
+        name: 'Luck Talisman',
+        group: 'minor'
     }, {
         id_prefix: 'waist',
-        name: 'Waist Talisman'
+        name: 'Waist Talisman',
+        group: 'minor'
     }, {
         id_prefix: 'occult',
-        name: 'Occult Talisman'
+        name: 'Occult Talisman',
+        group: 'minor'
     }]
 }
 
 custom_gear_data = {
     "head": {
         heal_dps: {
-            ql0: {
+            'ql0': {
                 rating: 559
             },
-            ql1: {
+            'ql1': {
                 rating: 596
             },
-            ql2: {
+            'ql2': {
                 rating: 636
             },
-            ql3: {
+            'ql3': {
                 rating: 682
             },
-            ql4: {
+            'ql4': {
                 rating: 735
             },
-            ql5: {
+            'ql5': {
                 rating: 788
             }
         },
         tank: {
-            ql0: {
+            'ql0': {
                 hitpoints: 2100
             },
-            ql1: {
+            'ql1': {
                 hitpoints: 2194
             },
-            ql2: {
+            'ql2': {
                 hitpoints: 2288
             },
-            ql3: {
+            'ql3': {
                 hitpoints: 2382
             },
-            ql4: {
+            'ql4': {
                 hitpoints: 2476
             },
-            ql5: {
+            'ql5': {
                 hitpoints: 2570
             }
         }
     },
     "major": {
         heal_dps: {
-            ql0: {
+            'ql0': {
                 rating: 505
             },
-            ql1: {
+            'ql1': {
                 rating: 538
             },
-            ql2: {
+            'ql2': {
                 rating: 575
             },
-            ql3: {
+            'ql3': {
                 rating: 616
             },
-            ql4: {
+            'ql4': {
                 rating: 664
             },
-            ql5: {
+            'ql5': {
                 rating: 712
             }
         },
         tank: {
-            ql0: {
+            'ql0': {
                 hitpoints: 1897
             },
-            ql1: {
+            'ql1': {
                 hitpoints: 1982
             },
-            ql2: {
+            'ql2': {
                 hitpoints: 2067
             },
-            ql3: {
+            'ql3': {
                 hitpoints: 2152
             },
-            ql4: {
+            'ql4': {
                 hitpoints: 2237
             },
-            ql5: {
+            'ql5': {
                 hitpoints: 2322
             }
         }
     },
     "minor": {
         heal_dps: {
-            ql0: {
+            'ql0': {
                 rating: 325
             },
-            ql1: {
+            'ql1': {
                 rating: 346
             },
-            ql2: {
+            'ql2': {
                 rating: 369
             },
-            ql3: {
+            'ql3': {
                 rating: 396
             },
-            ql4: {
+            'ql4': {
                 rating: 427
             },
-            ql5: {
+            'ql5': {
                 rating: 458
             }
         },
         tank: {
-            ql0: {
+            'ql0': {
                 hitpoints: 1220
             },
-            ql1: {
+            'ql1': {
                 hitpoints: 1274
             },
-            ql2: {
+            'ql2': {
                 hitpoints: 1329
             },
-            ql3: {
+            'ql3': {
                 hitpoints: 1383
             },
-            ql4: {
+            'ql4': {
                 hitpoints: 1438
             },
-            ql5: {
+            'ql5': {
                 hitpoints: 1492
             }
         }
