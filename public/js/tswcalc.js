c246338a83c091dfb2d7546b748b80a7518ebfc4
Joakim Bjornstad <joakibj@gmail.com>
2013-05-08 16:14:04 +0200

    moved function out of document.ready

--- public/js/tswcalc.js
+++ public/js/tswcalc.js
@@ -2,15 +2,15 @@ $(document).ready(function() {
 
     render_container(data);
     $('#summary').scrollToFixed();
-
-    function render_container(data) {
-        dust.render("container", data,
-
-        function(err, out) {
-            if (err) {
-                console.log(err);
-            }
-            $('.container').html(out);
-        });
-    }
 });
+
+function render_container(data) {
+    dust.render("container", data,
+
+    function(err, out) {
+        if (err) {
+            console.log(err);
+        }
+        $('.container').html(out);
+    });
+}
