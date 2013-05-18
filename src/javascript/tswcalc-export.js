12e0bf1b3fc1793af657afd47d4f7552d0e96ed5
joakibj <joakibj@gmail.com>
2013-05-18 19:01:22 +0200

    Created basic state export

--- src/javascript/tswcalc-export.js
+++ src/javascript/tswcalc-export.js
@@ -29,6 +29,28 @@ function Export() {
     };
 
     this.startExportUrl = function() {
-        $('#export-textarea').html(window.location.href + '?calc=');
+        var slotStates = this.collectAllSlotStates();
+        console.log(slotStates);
+        $('#export-textarea').html(window.location.href + '?calc=' + btoa(slotStates));
+    };
+
+    this.collectAllSlotStates = function() {
+        var slotStates = '';
+        for (var i = 0; i < template_data.slots.length; i++) {
+            slotStates += this.collectSlotState(template_data.slots[i].id_prefix);
+        }
+        return slotStates;
+    };
+
+    this.collectSlotState = function(slotId) {
+        var slotState = '';
+        var suffixes = ['-ql', '-role', '-glyph-ql', '-primary-glyph', '-secondary-glyph'];
+        for (var i = 0; i < suffixes.length; i++) {
+            slotState += slotId + suffixes[i] + '=' + $('#' + slotId + suffixes[i]).val();
+            if (i < (suffixes.length - 1)) {
+                slotState += '&';
+            }
+        }
+        return slotState;
     };
 };
