e1fa69f12ba3862afd1307cc70e57ae405e3990b
Joakim Bjornstad <joakibj@gmail.com>
2013-05-27 16:19:43 +0200

    Added data set for black bullion costs

--- src/javascript/tswcalc-data.js
+++ src/javascript/tswcalc-data.js
@@ -78,6 +78,72 @@ template_data = {
     }]
 }
 
+bb_costs = {
+    "weapon": {
+        "10.0": {
+            cost: 30
+        },
+        "10.1": {
+            cost: 70
+        },
+        "10.2": {
+            cost: 110
+        },
+        "10.3": {
+            cost: 150
+        },
+        "10.4": {
+            cost: 190
+        },
+        "10.5": {
+            cost: 230,
+            criterion_upgrade: true
+        }
+    },
+    "talisman": {
+        "10.0": {
+            cost: 30
+        },
+        "10.1": {
+            cost: 50
+        },
+        "10.2": {
+            cost: 70
+        },
+        "10.3": {
+            cost: 90
+        },
+        "10.4": {
+            cost: 110
+        },
+        "10.5": {
+            cost: 130,
+            criterion_upgrade: true
+        }
+    },
+    "glyph": {
+        "10.0": {
+            cost: 30
+        },
+        "10.1": {
+            cost: 50
+        },
+        "10.2": {
+            cost: 70
+        },
+        "10.3": {
+            cost: 90
+        },
+        "10.4": {
+            cost: 110
+        },
+        "10.5": {
+            cost: 130,
+            astral_fuse: true
+        }
+    }
+}
+
 custom_gear_data = {
     "weapon": {
         '10.0': {
