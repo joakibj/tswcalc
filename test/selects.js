a3ea5f18a95e9b04d2e0c26dd4849efc7c825bc0
joakibj <joakibj@gmail.com>
2013-06-09 13:11:47 +0200

    Added more tests and more clean setup + teardown

--- test/selects.js
+++ test/selects.js
@@ -23,7 +23,8 @@ module('selects', {
         }
     },
     teardown: function() {
-
+        selectHandler = [];
+        $('#qunit-fixture').html(' ');
     }
 });
 
@@ -44,10 +45,7 @@ test('should not have weapon-role select in the DOM', 1, function() {
 });
 
 test('should have required event listeners for change on selects in the DOM', (selectSuffixes.length * template_data.slots.length) - 1, function() {
-    var selectHandler = [];
     for (var i = 0; i < template_data.slots.length; i++) {
-        selectHandler[template_data.slots[i].id_prefix] = new SelectHandler(template_data.slots[i]);
-        selectHandler[template_data.slots[i].id_prefix].initiate();
         for (var suffix = 0; suffix < selectSuffixes.length; suffix++) {
             if (template_data.slots[i].id_prefix == 'weapon' && selectSuffixes[suffix] == '-role') {} else {
                 ok($._data($('#' + template_data.slots[i].id_prefix + selectSuffixes[suffix]).get(0), 'events').change instanceof Array, template_data.slots[i].id_prefix + selectSuffixes[suffix] + ' change event exists');
@@ -57,61 +55,94 @@ test('should have required event listeners for change on selects in the DOM', (s
 });
 
 test('should have added signets to correct group', 4, function() {
-    ok($('#weapon-pick-signet option').size(), 16);
-    ok($('#head-pick-signet option').size(), 5);
-    ok($('#ring-pick-signet option').size(), 3);
-    ok($('#occult-pick-signet option').size(), 28);
+    // None option must be taken into account
+    equal($('#weapon-pick-signet option').size(), 16 + 1);
+    equal($('#head-pick-signet option').size(), 5 + 16 + 1); //weapon signets also fit in the head
+    equal($('#ring-pick-signet option').size(), 3 + 1);
+    equal($('#occult-pick-signet option').size(), 28 + 1);
 });
 
 test('should get role', 1, function() {
     $('#head-role').val('tank');
-    ok(selectHandler['head'].getRole(), 'tank');
+    equal(selectHandler['head'].getRole(), 'tank');
 });
 
 test('should get slot ql', 1, function() {
     $('#head-ql').val('10.3');
-    ok(selectHandler['head'].getQl(), '10.3');
+    equal(selectHandler['head'].getQl(), '10.3');
 });
 
 test('should get glyph ql', 1, function() {
     $('#head-glyph-ql').val('10.3');
-    ok(selectHandler['head'].getGlyphQl(), '10.3');
+    equal(selectHandler['head'].getGlyphQl(), '10.3');
 });
 
 test('should get glyphs', 2, function() {
     $('#head-primary-glyph').val('critical-rating');
     $('#head-secondary-glyph').val('defense-rating');
-    ok(selectHandler['head'].getGlyph('primary'), 'critical-rating', 'Primary glyph select');
-    ok(selectHandler['head'].getGlyph('secondary'), 'defense-rating', 'Secondary glyph select');
+    equal(selectHandler['head'].getGlyph('primary'), 'critical-rating', 'Primary glyph select');
+    equal(selectHandler['head'].getGlyph('secondary'), 'defense-rating', 'Secondary glyph select');
 });
 
 test('should get signet quality', 1, function() {
     $('#head-signet-quality').val('epic');
-    ok(selectHandler['head'].getSignetQuality(), 'epic');
+    equal(selectHandler['head'].getSignetQuality(), 'epic');
 });
 
 test('should get signet', 1, function() {
     $('#head-pick-signet').val('12');
-    ok(selectHandler['head'].getSignet(), '12');
+    equal(selectHandler['head'].getSignet(), '12');
 });
 
 test('should get signets for head', 1, function() {
-    ok(selectHandler['head'].getSignetsForHead('head').length, 5);
+    equal(selectHandler['head'].getSignetsForHead('head').length, 16);
 });
 
-test('should get signet value based on quality', 1, function() {
+test('should get single signet value based on quality', 1, function() {
     $('#head-pick-signet').val('12');
     $('#head-signet-quality').val('epic');
     var signet = signet_data.find('head', selectHandler['head'].getSignet());
 
-    ok(selectHandler['head'].determineSignetQualityValue(signet), 141);
+    equal(selectHandler['head'].determineSignetQualityValue(signet), 141);
 });
 
+test('should get indexed signet value based on quality', 2, function() {
+    $('#occult-pick-signet').val('47');
+    $('#occult-signet-quality').val('epic');
+    var signet = signet_data.find('minor', selectHandler['occult'].getSignet());
 
+    equal(selectHandler['occult'].determineSignetQualityValue(signet, 0), 150);
+    equal(selectHandler['occult'].determineSignetQualityValue(signet, 1), 57);
+});
 
+test('should get signet description for single value replace', 1, function() {
+    $('#head-pick-signet').val('12');
+    $('#head-signet-quality').val('epic');
+    var signet = signet_data.find('head', selectHandler['head'].getSignet());
 
+    equal(selectHandler['head'].getSignetDescription(signet), 'When you hit a weakened target you have a 5% chance to be healed for 141 every 2 seconds for 8 seconds.');
+});
 
+test('should get signet description for multiple value replace', 1, function() {
+    $('#occult-pick-signet').val('47');
+    $('#occult-signet-quality').val('epic');
+    var signet = signet_data.find('minor', selectHandler['occult'].getSignet());
 
+    equal(selectHandler['occult'].getSignetDescription(signet), 'When your health is below 50% you are healed for 150 and affected by a heal over time effect healing you for 57 every 2 seconds for 8 seconds.');
+});
 
+test('should update signet description', 1, function() {
+    $('#head-pick-signet').val('12');
+    $('#head-signet-quality').val('epic');
+    selectHandler['head'].updateSignetDescription();
 
+    equal($('#head-signet-description').html(), 'When you hit a weakened target you have a 5% chance to be healed for 141 every 2 seconds for 8 seconds.');
+});
 
+test('should update signet description to blank if no signet is picked', 1, function() {
+    $('#head-pick-signet').val('none');
+    $('#head-signet-quality').val('normal');
+    selectHandler['head'].updateSignetDescription();
+
+    equal($('#head-signet-description').html(), '');
+});
