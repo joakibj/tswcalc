baa667fc7778265ad0fd59daa8d7cd101c1d5240
joakibj <joakibj@gmail.com>
2013-06-12 21:59:37 +0200

    Added tests and more functionality for slots

--- src/javascript/tswcalc-slots.js
+++ src/javascript/tswcalc-slots.js
@@ -7,9 +7,13 @@ function Slot(id, name, group) {
     this.el = {
         role: $('#' + this.id + '-role'),
         ql: $('#' + this.id + '-ql'),
-        glyphql: $('#' + this.id + '-glyph-ql'),
-        primaryglyph: $('#' + this.id + '-primary-glyph'),
-        secondaryglyph: $('#' + this.id + '-secondary-glyph')
+        glyphQl: $('#' + this.id + '-glyph-ql'),
+        primaryGlyph: $('#' + this.id + '-primary-glyph'),
+        secondaryGlyph: $('#' + this.id + '-secondary-glyph'),
+        primaryDist: '#' + this.id + '-primary-glyph-dist > button.btn.active',
+        secondaryDist: '#' + this.id + '-secondary-glyph-dist > button.btn.active',
+        signetId: $('#' + this.id + '-pick-signet'),
+        signetQuality: $('#' + this.id + '-signet-quality')
     };
 
     this.role = function() {
@@ -20,16 +24,83 @@ function Slot(id, name, group) {
         return this.el.ql.val();
     };
 
-    this.glyphql = function() {
-        return this.el.glyphql.val();
+    this.glyphQl = function() {
+        return this.el.glyphQl.val();
     };
 
-    this.primaryglyph = function() {
-        return this.el.primaryglyph.val();
+    this.primaryGlyph = function() {
+        return this.el.primaryGlyph.val();
     };
 
-    this.secondaryglyph = function() {
-        return this.el.secondaryglyph.val();
+    this.secondaryGlyph = function() {
+        return this.el.secondaryGlyph.val();
+    };
+
+    this.primaryDist = function() {
+        return $(this.el.primaryDist)[0].innerHTML;
+    };
+
+    this.secondaryDist = function() {
+        return $(this.el.secondaryDist)[0].innerHTML;
+    };
+
+    this.signetId = function() {
+        return this.el.signetId.val();
+    };
+
+    this.signetQuality = function() {
+        return this.el.signetQuality.val();
+    };
+
+    this.signet = function() {
+        return signet_data.find(this.group, this.signetId());
+    };
+
+    this.state = function() {
+        return {
+            role: this.role(),
+            ql: this.ql(),
+            glyph_ql: this.glyphQl(),
+            primary_glyph: this.primaryGlyph(),
+            secondary_glyph: this.secondaryGlyph(),
+            primary_dist: this.primaryDist(),
+            secondary_dist: this.secondaryDist(),
+            signet_quality: this.signetQuality(),
+            signet_id: this.signetId()
+        };
+    };
+
+    this.mappedState = function() {
+        return {
+            role: this.stripContent(this.role()),
+            ql: this.stripContent(this.ql()),
+            glyph_ql: this.stripContent(this.glyphQl()),
+            primary_glyph: this.stripContent(this.primaryGlyph()),
+            secondary_glyph: this.stripContent(this.secondaryGlyph()),
+            primary_dist: this.primaryDist(),
+            secondary_dist: this.secondaryDist(),
+            signet_quality: this.stripContent(this.signetQuality()),
+            signet_id: this.stripContent(this.signetId())
+        };
+    };
+
+    this.stripContent = function(val) {
+        if (val == null || val == 'none') {
+            val = 0;
+        }
+
+        var qlpattern = /\d+\.\d/;
+        if (val != 0 && val.match(qlpattern)) {
+            return val.split('.')[1];
+        } else if ($.inArray(val, Object.keys(stat_mapping.to_num)) != -1) {
+            return stat_mapping.to_num[val];
+        } else if ($.inArray(val, Object.keys(role_mapping.to_num)) != -1) {
+            return role_mapping.to_num[val];
+        } else if ($.inArray(val, Object.keys(signet_quality_mapping.to_num)) != -1) {
+            return signet_quality_mapping.to_num[val];
+        } else {
+            return val;
+        }
     };
 }
 
@@ -40,5 +111,23 @@ var slots = {
             var slotData = template_data.slots[i];
             this[slotData.id_prefix] = new Slot(slotData.id_prefix, slotData.name, slotData.group);
         }
+    },
+
+    state: function() {
+        var slotStates = {};
+        for (var i = 0; i < template_data.slots.length; i++) {
+            var slot = template_data.slots[i];
+            slotStates[slot.id_prefix] = this[slot.id_prefix].state();
+        }
+        return slotStates;
+    },
+
+    mappedState: function() {
+        var mappedSlotStates = {};
+        for (var i = 0; i < template_data.slots.length; i++) {
+            var slot = template_data.slots[i];
+            mappedSlotStates[slot.id_prefix] = this[slot.id_prefix].mappedState();
+        }
+        return mappedSlotStates;
     }
 };
--- test/slots.js
+++ test/slots.js
@@ -1,6 +1,10 @@
+var summary = {};
 module('slots-unit-tests', {
     setup: function() {
+        summary = new Summary();
         renderSlots();
+        initiateSelectHandlers();
+        initiateButtonHandlers();
         slots.init();
         createTankBuild();
     }
@@ -15,11 +19,85 @@ test('should get talisman ql', 1, function() {
 });
 
 test('should get talisman glyph ql', 1, function() {
-    equal(slots.head.glyphql(), '10.5');
+    equal(slots.head.glyphQl(), '10.5');
 });
 
 test('should get talisman glyphs', 2, function() {
-    equal(slots.head.primaryglyph(), 'block-rating');
-    equal(slots.head.secondaryglyph(), 'none');
+    equal(slots.head.primaryGlyph(), 'block-rating');
+    equal(slots.head.secondaryGlyph(), 'none');
+});
+
+test('should get talisman glyph distribution', 2, function() {
+    equal(slots.head.primaryDist(), '4');
+    equal(slots.head.secondaryDist(), '0');
+});
+
+test('should get talisman signet id and quality', 2, function() {
+    equal(slots.head.signetId(), '18');
+    equal(slots.head.signetQuality(), 'epic');
+});
+
+test('should get talisman signet object', 1, function() {
+    deepEqual(slots.head.signet(), signet_data.find('head', '18'));
+});
+
+test('should get talisman signet None object if signet Id cannot be found', 1, function() {
+    deepEqual(slots.wrist.signet(), signet_data.noneSignet());
+});
+
+test('should collect current slot state', 9, function() {
+    var slotState = slots.head.state();
+
+    deepEqual(slotState.ql, '10.4');
+    deepEqual(slotState.role, 'tank');
+    deepEqual(slotState.glyph_ql, '10.5');
+    deepEqual(slotState.primary_glyph, 'block-rating');
+    deepEqual(slotState.secondary_glyph, 'none');
+    deepEqual(slotState.primary_dist, '4');
+    deepEqual(slotState.secondary_dist, '0');
+    deepEqual(slotState.signet_quality, 'epic');
+    deepEqual(slotState.signet_id, '18');
+});
+
+test('should collect current mapped slot state', 9, function() {
+    var slotState = slots.head.mappedState();
+
+    deepEqual(slotState.ql, '4');
+    deepEqual(slotState.role, 1);
+    deepEqual(slotState.glyph_ql, '5');
+    deepEqual(slotState.primary_glyph, 5);
+    deepEqual(slotState.secondary_glyph, 0);
+    deepEqual(slotState.primary_dist, '4');
+    deepEqual(slotState.secondary_dist, '0');
+    deepEqual(slotState.signet_quality, 3);
+    deepEqual(slotState.signet_id, '18');
+});
+
+test('should collect all slot states', 9, function() {
+    var slotStates = slots.state();
+
+    deepEqual(slotStates.head.ql, '10.4');
+    deepEqual(slotStates.head.role, 'tank');
+    deepEqual(slotStates.head.glyph_ql, '10.5');
+    deepEqual(slotStates.head.primary_glyph, 'block-rating');
+    deepEqual(slotStates.head.secondary_glyph, 'none');
+    deepEqual(slotStates.head.primary_dist, '4');
+    deepEqual(slotStates.head.secondary_dist, '0');
+    deepEqual(slotStates.head.signet_quality, 'epic');
+    deepEqual(slotStates.head.signet_id, '18');
+});
+
+test('should collect all mapped slot states', 9, function() {
+    var slotStates = slots.mappedState();
+
+    deepEqual(slotStates.head.ql, '4');
+    deepEqual(slotStates.head.role, 1);
+    deepEqual(slotStates.head.glyph_ql, '5');
+    deepEqual(slotStates.head.primary_glyph, 5);
+    deepEqual(slotStates.head.secondary_glyph, 0);
+    deepEqual(slotStates.head.primary_dist, '4');
+    deepEqual(slotStates.head.secondary_dist, '0');
+    deepEqual(slotStates.head.signet_quality, 3);
+    deepEqual(slotStates.head.signet_id, '18');
 });
 
