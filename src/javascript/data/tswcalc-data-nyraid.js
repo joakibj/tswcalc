9d8cb65307ac9da29ce616ae9203b1e087f596d4
Joakim Bjornstad <joakibj@gmail.com>
2013-06-19 13:50:56 +0200

    Treating NY item effect as signets

--- src/javascript/data/tswcalc-data-nyraid.js
+++ src/javascript/data/tswcalc-data-nyraid.js
@@ -2,81 +2,156 @@ ny_raid_items = {
     'head': {
         heal: {
             name: 'Blood of the Old Ones',
-            description: 'Whenever you apply a heal effect to a friend, there is a 10% chance that you will heal the target for an additional 3% of their maximum health. This can only happen once every 5 seconds.',
-            cooldown: 5,
-            stack_interval: 0,
-            stack_max: 0
+            signet: {
+                id: 80,
+                name: 'Item: Blood of the Old Ones',
+                description: 'Whenever you apply a heal effect to a friend, there is a %0% chance that you will heal the target for an additional %1% of their maximum health. This can only happen once every 5 seconds.',
+                cooldown: 5,
+                quality: [{
+                        epic: 10
+                    }, {
+                        epic: 3
+                    }
+                ],
+                icon: 'blood_of_the_old_ones'
+            }
         },
         tank: {
             name: 'Mark of the Starspawn',
-            description: 'Whenever you are attacked, you have a 5% chance to gain a beneficial effect that reduces damage received from penetrating attacks for 15 seconds. The duration of this effect is not refreshed when reapplied.',
-            cooldown: 0,
-            stack_interval: 0,
-            stack_max: 0
+            signet: {
+                id: 81,
+                name: 'Item: Mark of the Starspawn',
+                description: 'Whenever you are attacked, you have a %d% chance to gain a beneficial effect that reduces damage received from penetrating attacks for 15 seconds. The duration of this effect is not refreshed when reapplied.',
+                cooldown: 0,
+                quality: {
+                    epic: 5
+                },
+                icon: 'mark_of_the_starspawn'
+            }
         },
         dps: {
             name: 'Ashes of Elder Things',
-            description: 'Whenever you penetrate, you gain a beneficial effect that increases your Penetration Rating by 25% for 15 seconds. The duration of this effect is not refreshed when reapplied.',
-            cooldown: 0,
-            stack_interval: 0,
-            stack_max: 0
+            signet: {
+                id: 82,
+                name: 'Item: Ashes of Elder Things',
+                description: 'Whenever you penetrate, you gain a beneficial effect that increases your Penetration Rating by %d% for 15 seconds. The duration of this effect is not refreshed when reapplied.',
+                cooldown: 0,
+                quality: {
+                    epic: 25
+                },
+                bonus: {
+                    stat: ['penetration-rating'],
+                    multiply: 1.25
+                },
+                icon: 'ashes_of_elder_things'
+            }
         }
     },
     'ring': {
         heal: {
             name: 'Coney Island Band',
-            description: 'Whenever you critically heal, you gain a beneficial effect that increases your Crit Power by 25% for 15 seconds. The duration of this effect is not refreshed when reapplied.',
-            cooldown: 0,
-            stack_interval: 0,
-            stack_max: 0
+            signet: {
+                id: 83,
+                name: 'Item: Coney Island Band',
+                description: 'Whenever you critically heal, you gain a beneficial effect that increases your Crit Power by %d% for 15 seconds. The duration of this effect is not refreshed when reapplied.',
+                cooldown: 0,
+                quality: {
+                    epic: 25
+                },
+                bonus: {
+                    stat: ['critical-power-percentage'],
+                    add: 25
+                },
+                icon: 'coney_island_band'
+            }
         }
     },
     'neck': {
         dps: {
             name: 'Egon Pendant',
-            description: 'Whenever you critically hit, you gain a beneficial effect that increases the effectiveness of your Critical Rating for 12 seconds. The duration of this effect is not refreshed when reapplied.',
-            cooldown: 0,
-            stack_interval: 0,
-            stack_max: 0
+            signet: {
+                id: 84,
+                name: 'Item: Egon Pendant',
+                description: 'Whenever you critically hit, you gain a beneficial effect that increases the effectiveness of your Critical Rating for 12 seconds. The duration of this effect is not refreshed when reapplied.',
+                cooldown: 0,
+                quality: {
+                    epic: 0
+                },
+                icon: 'egon_pendant'
+            }
         }
     },
     'wrist': {
         tank: {
             name: 'Brooklyn Bracer',
-            description: 'Whenever you are attacked, you gain a single stack of a beneficial effect that increases your Evade Chance by 5% per stack. This effect is removed after you successfully evade.',
-            cooldown: 6,
-            stack_interval: 0,
-            stack_max: 0
+            signet: {
+                id: 85,
+                name: 'Item: Brooklyn Bracer',
+                description: 'Whenever you are attacked, you gain a single stack of a beneficial effect that increases your Evade Chance by %d% per stack. This effect is removed after you successfully evade.',
+                cooldown: 6,
+                quality: {
+                    epic: 5
+                },
+                icon: 'brooklyn_bracer'
+            }
         }
     },
     'luck': {
         dps: {
             name: 'Subway Tokens',
             description: 'Every 6 seconds, you gain a single stack of a beneficial effect that increases all damage dealt by 1% per stack. This effect is removed whenever you glance or the number of stacks exceeds 10.',
-            cooldown: 0,
+            signet: {
+                id: 86,
+                name: 'Item: Subway Tokens',
+                description: 'Every 6 seconds, you gain a single stack of a beneficial effect that increases all damage dealt by 1% per stack. This effect is removed whenever you glance or the number of stacks exceeds 10.',
+                cooldown: 6,
+                quality: {
+                    epic: 0
+                },
+                icon: 'subway_tokens'
+            },
+            bonus: {
+                stat: ['damage-deal-percentage'],
+                add: 1
+            },
             stack_interval: 6,
-            stack_max: 10,
-            stack_value: 1
+            stack_max: 10
         }
     },
     'waist': {
         tank: {
             name: 'NY Buckle',
-            description: 'Every 6 seconds, you gain a single stack of a beneficial effect that increases your Physical and Magical Protection by 3% per stack. This effect is removed whenever you are penetrated or the number of stacks exceeds 10.',
-            cooldown: 0,
+            signet: {
+                id: 87,
+                name: 'Item: NY Buckle',
+                description: 'Every 6 seconds, you gain a single stack of a beneficial effect that increases your Physical and Magical Protection by 3% per stack. This effect is removed whenever you are penetrated or the number of stacks exceeds 10.',
+                cooldown: 6,
+                quality: {
+                    epic: 0
+                },
+                icon: 'ny_buckle'
+            },
+            bonus: {
+                stat: ['physical-protection', 'magical-protection'],
+                multiply: 1.03
+            },
             stack_interval: 6,
-            stack_max: 10,
-            stack_value: 3
+            stack_max: 10
         }
     },
     'occult': {
         heal: {
             name: 'Broadway Charm',
-            description: 'Whenever you heal a target that is below 66% of their health, there is a 33% chance that you will also give them a beneficial effect that increases their Physical and Magical Protection by 400 for 10 seconds.',
-            cooldown: 0,
-            stack_interval: 6,
-            stack_max: 10,
-            stack_value: 1
+            signet: {
+                id: 88,
+                name: 'Item: Broadway Charm',
+                description: 'Whenever you heal a target that is below 66% of their health, there is a 33% chance that you will also give them a beneficial effect that increases their Physical and Magical Protection by 400 for 10 seconds.',
+                cooldown: 6,
+                quality: {
+                    epic: 0
+                },
+                icon: 'broadway_charm'
+            }
         }
     }
 };
