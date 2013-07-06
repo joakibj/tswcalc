b9f544831f404089aad008ecf8b18ab9d17de4e1
joakibj <joakibj@gmail.com>
2013-07-06 23:46:39 +0200

    Add test to support 0.4 URL

--- test/import.js
+++ test/import.js
@@ -11,6 +11,128 @@ module('import-integration-tests', {
     }
 });
 
+test('should import URL and set summary and slots for 0.4.0 links', 95, function() {
+    var vars = {
+        head: '4,1,5,5,0,4,0',
+        luck: '4,3,4,8,0,4,0',
+        neck: '4,1,5,5,0,4,0',
+        occult: '4,3,4,4,0,4,0',
+        ring: '4,3,4,6,0,4,0',
+        waist: '4,1,4,8,0,4,0,3,87',
+        weapon: '5,0,4,4,0,4,0',
+        wrist: '4,1,4,6,0,4,0,3,85'
+    };
+
+    tswcalc.import.start(vars);
+
+    // Summary
+    equal($('#stat-hitpoints').html(), '10358');
+    equal($('#stat-combat-power').html(), '496');
+    equal($('#stat-attack-rating').html(), '1518');
+    equal($('#stat-weapon-power').html(), '457');
+    equal($('#stat-heal-rating').html(), '0');
+    equal($('#stat-critical-rating').html(), '0');
+    equal($('#stat-critical-chance').html(), '5.00 %');
+    equal($('#stat-critical-power').html(), '0');
+    equal($('#stat-critical-power-percentage').html(), '25.0 %');
+    equal($('#stat-penetration-rating').html(), '0');
+    equal($('#stat-hit-rating').html(), '+504');
+    equal($('#stat-block-rating').html(), '+691');
+    equal($('#stat-defense-rating').html(), '+576');
+    equal($('#stat-evade-rating').html(), '0');
+    equal($('#stat-physical-protection').html(), '+609');
+    equal($('#stat-magical-protection').html(), '+249');
+
+    equal($('#weapon-ql').val(), '10.5');
+    equal($('#weapon-glyph-ql').val(), '10.4');
+    equal($('#weapon-primary-glyph').val(), 'hit-rating');
+    equal($('#weapon-secondary-glyph').val(), 'none');
+    ok($('#weapon-primary-glyph-dist-btn4').hasClass('active'));
+    ok($('#weapon-secondary-glyph-dist-btn0').hasClass('active'));
+    equal($('#weapon-signet-quality').val(), 'none');
+    equal($('#weapon-pick-signet').val(), 'none');
+
+    equal($('#head-ql').val(), '10.4');
+    equal($('#head-role').val(), 'tank');
+    equal($('#head-glyph-ql').val(), '10.5');
+    equal($('#head-primary-glyph').val(), 'block-rating');
+    equal($('#head-secondary-glyph').val(), 'none');
+    ok($('#head-primary-glyph-dist-btn4').hasClass('active'));
+    ok($('#head-secondary-glyph-dist-btn0').hasClass('active'));
+    equal($('#head-signet-quality').val(), 'none');
+    equal($('#head-pick-signet').val(), 'none');
+
+    equal($('#ring-ql').val(), '10.4');
+    equal($('#ring-role').val(), 'dps');
+    equal($('#ring-glyph-ql').val(), '10.4');
+    equal($('#ring-primary-glyph').val(), 'defense-rating');
+    equal($('#ring-secondary-glyph').val(), 'none');
+    ok($('#ring-primary-glyph-dist-btn4').hasClass('active'));
+    ok($('#ring-secondary-glyph-dist-btn0').hasClass('active'));
+    equal($('#ring-signet-quality').val(), 'none');
+    equal($('#ring-pick-signet').val(), 'none');
+
+    equal($('#neck-ql').val(), '10.4');
+    equal($('#neck-role').val(), 'tank');
+    equal($('#neck-glyph-ql').val(), '10.5');
+    equal($('#neck-primary-glyph').val(), 'block-rating');
+    equal($('#neck-secondary-glyph').val(), 'none');
+    ok($('#neck-primary-glyph-dist-btn4').hasClass('active'));
+    ok($('#neck-secondary-glyph-dist-btn0').hasClass('active'));
+    equal($('#neck-signet-quality').val(), 'none');
+    equal($('#neck-pick-signet').val(), 'none');
+
+    //Brooklyn Bracer
+    equal($('#wrist-ql').val(), '10.4');
+    equal($('#wrist-role').val(), 'tank');
+    equal($('#wrist-glyph-ql').val(), '10.4');
+    equal($('#wrist-primary-glyph').val(), 'defense-rating');
+    equal($('#wrist-secondary-glyph').val(), 'none');
+    ok($('#wrist-primary-glyph-dist-btn4').hasClass('active'));
+    ok($('#wrist-secondary-glyph-dist-btn0').hasClass('active'));
+    equal($('#wrist-signet-quality').val(), 'epic');
+    equal($('#wrist-pick-signet').val(), '85');
+    ok($('#wrist-signet-quality').attr('disabled'));
+    ok($('#wrist-pick-signet').attr('disabled'));
+    ok($('#wrist-ql').attr('disabled'));
+    ok(!$('#wrist-nyraid').attr('disabled'));
+
+    equal($('#luck-ql').val(), '10.4');
+    equal($('#luck-role').val(), 'dps');
+    equal($('#luck-glyph-ql').val(), '10.4');
+    equal($('#luck-primary-glyph').val(), 'physical-protection');
+    equal($('#luck-secondary-glyph').val(), 'none');
+    ok($('#luck-primary-glyph-dist-btn4').hasClass('active'));
+    ok($('#luck-secondary-glyph-dist-btn0').hasClass('active'));
+    equal($('#luck-signet-quality').val(), 'none');
+    equal($('#luck-pick-signet').val(), 'none');
+
+    //NY Buckle
+    equal($('#waist-ql').val(), '10.4');
+    equal($('#waist-role').val(), 'tank');
+    equal($('#waist-glyph-ql').val(), '10.4');
+    equal($('#waist-primary-glyph').val(), 'physical-protection');
+    equal($('#waist-secondary-glyph').val(), 'none');
+    ok($('#waist-primary-glyph-dist-btn4').hasClass('active'));
+    ok($('#waist-secondary-glyph-dist-btn0').hasClass('active'));
+    equal($('#waist-signet-quality').val(), 'epic');
+    equal($('#waist-pick-signet').val(), '87');
+    ok($('#waist-signet-quality').attr('disabled'));
+    ok($('#waist-pick-signet').attr('disabled'));
+    ok($('#waist-ql').attr('disabled'));
+    ok(!$('#waist-nyraid').attr('disabled'));
+
+    equal($('#occult-ql').val(), '10.4');
+    equal($('#occult-role').val(), 'dps');
+    equal($('#occult-glyph-ql').val(), '10.4');
+    equal($('#occult-primary-glyph').val(), 'hit-rating');
+    equal($('#occult-secondary-glyph').val(), 'none');
+    ok($('#occult-primary-glyph-dist-btn4').hasClass('active'));
+    ok($('#occult-secondary-glyph-dist-btn0').hasClass('active'));
+    equal($('#occult-signet-quality').val(), 'none');
+    equal($('#occult-pick-signet').val(), 'none');
+});
+
 test('should import URL and set summary and slots', 95, function() {
     var vars = {
         head: '4,1,5,5,0,4,0,3,18',
