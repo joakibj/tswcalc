46f1d24822c0115cc85d94fb1bdc9c5e1986d491
joakibj <joakibj@gmail.com>
2013-07-29 02:02:28 +0200

    Made test task mirror default (requires build)

--- Gruntfile.js
+++ Gruntfile.js
@@ -164,6 +164,6 @@ module.exports = function(grunt) {
 
   grunt.registerTask('build', ['dust', 'concat', 'replace:develop', 'copy:develop']);
   grunt.registerTask('default', ['build', 'qunit']);
-  grunt.registerTask('test', ['qunit']);
+  grunt.registerTask('test', ['default']);
   grunt.registerTask('dist', ['dust', 'concat', 'uglify', 'replace:dist', 'copy:develop', 'copy:dist', 'qunit']);
 };
