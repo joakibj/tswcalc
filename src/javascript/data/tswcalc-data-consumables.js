37aec1088d5b051059912fcbf79c03ac216bbf9c
joakibj <joakibj@gmail.com>
2013-08-28 20:33:57 +0200

    Tabs to spaces

--- src/javascript/data/tswcalc-data-consumables.js
+++ src/javascript/data/tswcalc-data-consumables.js
@@ -2,115 +2,115 @@ var tswcalc = tswcalc || {};
 tswcalc.data = tswcalc.data || {};
 
 tswcalc.data.consumables = {
-	pure_anima: {
-		'none': {
-			name: 'None',
-			description: '',
-			bonus: {}
-		},
-		'health': {
-			name: 'Max Health',
-			description: 'Grants 940 Max Health for 60 minutes. Persists through death.',
-			bonus: {
-				stat: 'hitpoints',
-				add: 940
-			}
+    pure_anima: {
+        'none': {
+            name: 'None',
+            description: '',
+            bonus: {}
+        },
+        'health': {
+            name: 'Max Health',
+            description: 'Grants 940 Max Health for 60 minutes. Persists through death.',
+            bonus: {
+                stat: 'hitpoints',
+                add: 940
+            }
 
-		},
-		'attack-rating': {
-			name: 'Attack Rating',
-			description: 'Grants 240 Attack Rating for 60 minutes. Persists through death.',
-			bonus: {
-				stat: 'attack-rating',
-				add: 240
-			}
-		},
-		'heal-rating': {
-			name: 'Heal Rating',
-			description: 'Grants 240 Heal Rating for 60 minutes. Persists through death.',
-			bonus: {
-				stat: 'heal-rating',
-				add: 240
-			}
-		}
-	},
-	anima: {
-		'none': {
-			name: 'None',
-			description: '',
-			bonus: {}
-		},
-		'critical-rating': {
-			name: 'Crit Rating',
-			description: 'Grants 119 Crit Rating for 60 minutes. Removed on death.',
-			bonus: {
-				stat: 'critical-rating',
-				add: 119
-			}
-		},
-		'critical-power': {
-			name: 'Crit Damage Rating',
-			description: 'Grants 100 Crit Damage Rating for 60 minutes. Removed on death.',
-			bonus: {
-				stat: 'critical-power',
-				add: 100
-			}
-		},
-		'penetration-rating': {
-			name: 'Penetration Rating',
-			description: 'Grants 100 Penetration Rating for 60 minutes. Removed on death.',
-			bonus: {
-				stat: 'penetration-rating',
-				add: 100
-			}
-		},
-		'hit-rating': {
-			name: 'Hit Rating',
-			description: 'Grants 100 Hit Rating for 60 minutes. Removed on death.',
-			bonus: {
-				stat: 'hit-rating',
-				add: 100
-			}
-		},
-		'block-rating': {
-			name: 'Block Rating',
-			description: 'Grants 100 Block Rating for 60 minutes. Removed on death.',
-			bonus: {
-				stat: 'block-rating',
-				add: 100
-			}
-		},
-		'defense-rating': {
-			name: 'Defense Rating',
-			description: 'Grants 100 Defense Rating for 60 minutes. Removed on death.',
-			bonus: {
-				stat: 'defense-rating',
-				add: 100
-			}
-		},
-		'evade-rating': {
-			name: 'Evade Rating',
-			description: 'Grants 110 Evade Rating for 60 minutes. Removed on death.',
-			bonus: {
-				stat: 'evade-rating',
-				add: 110
-			}
-		},
-		'physical-protection': {
-			name: 'Physical Protection Rating',
-			description: 'Grants 66 Physical Protection Rating for 60 minutes. Removed on death.',
-			bonus: {
-				stat: 'physical-protection',
-				add: 66
-			}
-		},
-		'magical-protection': {
-			name: 'Magical Protection Rating',
-			description: 'Grants 100 Magical Protection Rating for 60 minutes. Removed on death.',
-			bonus: {
-				stat: 'magical-protection',
-				add: 100
-			}
-		}
-	}
+        },
+        'attack-rating': {
+            name: 'Attack Rating',
+            description: 'Grants 240 Attack Rating for 60 minutes. Persists through death.',
+            bonus: {
+                stat: 'attack-rating',
+                add: 240
+            }
+        },
+        'heal-rating': {
+            name: 'Heal Rating',
+            description: 'Grants 240 Heal Rating for 60 minutes. Persists through death.',
+            bonus: {
+                stat: 'heal-rating',
+                add: 240
+            }
+        }
+    },
+    anima: {
+        'none': {
+            name: 'None',
+            description: '',
+            bonus: {}
+        },
+        'critical-rating': {
+            name: 'Crit Rating',
+            description: 'Grants 119 Crit Rating for 60 minutes. Removed on death.',
+            bonus: {
+                stat: 'critical-rating',
+                add: 119
+            }
+        },
+        'critical-power': {
+            name: 'Crit Damage Rating',
+            description: 'Grants 100 Crit Damage Rating for 60 minutes. Removed on death.',
+            bonus: {
+                stat: 'critical-power',
+                add: 100
+            }
+        },
+        'penetration-rating': {
+            name: 'Penetration Rating',
+            description: 'Grants 100 Penetration Rating for 60 minutes. Removed on death.',
+            bonus: {
+                stat: 'penetration-rating',
+                add: 100
+            }
+        },
+        'hit-rating': {
+            name: 'Hit Rating',
+            description: 'Grants 100 Hit Rating for 60 minutes. Removed on death.',
+            bonus: {
+                stat: 'hit-rating',
+                add: 100
+            }
+        },
+        'block-rating': {
+            name: 'Block Rating',
+            description: 'Grants 100 Block Rating for 60 minutes. Removed on death.',
+            bonus: {
+                stat: 'block-rating',
+                add: 100
+            }
+        },
+        'defense-rating': {
+            name: 'Defense Rating',
+            description: 'Grants 100 Defense Rating for 60 minutes. Removed on death.',
+            bonus: {
+                stat: 'defense-rating',
+                add: 100
+            }
+        },
+        'evade-rating': {
+            name: 'Evade Rating',
+            description: 'Grants 110 Evade Rating for 60 minutes. Removed on death.',
+            bonus: {
+                stat: 'evade-rating',
+                add: 110
+            }
+        },
+        'physical-protection': {
+            name: 'Physical Protection Rating',
+            description: 'Grants 66 Physical Protection Rating for 60 minutes. Removed on death.',
+            bonus: {
+                stat: 'physical-protection',
+                add: 66
+            }
+        },
+        'magical-protection': {
+            name: 'Magical Protection Rating',
+            description: 'Grants 100 Magical Protection Rating for 60 minutes. Removed on death.',
+            bonus: {
+                stat: 'magical-protection',
+                add: 100
+            }
+        }
+    }
 };
--- src/javascript/tswcalc-miscslot.js
+++ src/javascript/tswcalc-miscslot.js
@@ -1,48 +1,50 @@
 var tswcalc = tswcalc || {};
 
 tswcalc.miscslot = function() {
-	var el = {};
-	var elInit = function() {
-		return {
-			pure_anima: {
-				select: $('#pure-anima'),
-				description: $('#pure-anima-description')
-			},
-			anima: {
-				select: $('#anima'),
-				description: $('#anima-description')
-			}
-		};
-	};
+    var el = {};
+    var elInit = function() {
+        return {
+            pure_anima: {
+                select: $('#pure-anima'),
+                description: $('#pure-anima-description')
+            },
+            anima: {
+                select: $('#anima'),
+                description: $('#anima-description')
+            }
+        };
+    };
 
-	var init = function() {
-		el = elInit();
-		bindEvents();
-	};
+    var init = function() {
+        el = elInit();
+        bindEvents();
+    };
 
-	var bindEvents = function() {
-		el.pure_anima.select.on('change', pureAnimaChange);
-		el.anima.select.on('change', animaChange);
-	};
+    var bindEvents = function() {
+        el.pure_anima.select.on('change', pureAnimaChange);
+        el.anima.select.on('change', animaChange);
+    };
 
-	var pureAnimaChange = function(event) {
-		changeDescription('pure_anima', el.pure_anima.select.val());
-	};
+    var pureAnimaChange = function(event) {
+        changeDescription('pure_anima', el.pure_anima.select.val());
+    };
 
-	var animaChange = function(event) {
-		changeDescription('anima', el.anima.select.val());
-	};
+    var animaChange = function(event) {
+        changeDescription('anima', el.anima.select.val());
+    };
 
-	var changeDescription = function(anima, selected) {
-		var consumable = tswcalc.data.consumables[anima][selected];
-		if(consumable !== undefined) {
-			el[anima].description.html(consumable.description);
-		}
-	};
+    var changeDescription = function(anima, selected) {
+        var consumable = tswcalc.data.consumables[anima][selected];
+        if (consumable !== undefined) {
+            el[anima].description.html(consumable.description);
+            updatePrimaryStats();
+            updateOffensiveDefensiveStats();
+        }
+    };
 
-	var oPublic = {
-		init: init,
-	};
+    var oPublic = {
+        init: init,
+    };
 
-	return oPublic;
+    return oPublic;
 }();
