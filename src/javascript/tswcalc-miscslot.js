3f86d6d514dd03174dd5a6778ea3371c390503d2
joakibj <joakibj@gmail.com>
2013-08-28 20:28:19 +0200

    Change anima/pure anima select
    Added event handler for change event
    Changes description

--- src/javascript/tswcalc-miscslot.js
+++ src/javascript/tswcalc-miscslot.js
@@ -1,8 +1,43 @@
 var tswcalc = tswcalc || {};
 
 tswcalc.miscslot = function() {
-	var init = function() {
+	var el = {};
+	var elInit = function() {
+		return {
+			pure_anima: {
+				select: $('#pure-anima'),
+				description: $('#pure-anima-description')
+			},
+			anima: {
+				select: $('#anima'),
+				description: $('#anima-description')
+			}
+		};
+	};
 
+	var init = function() {
+		el = elInit();
+		bindEvents();
+	};
+
+	var bindEvents = function() {
+		el.pure_anima.select.on('change', pureAnimaChange);
+		el.anima.select.on('change', animaChange);
+	};
+
+	var pureAnimaChange = function(event) {
+		changeDescription('pure_anima', el.pure_anima.select.val());
+	};
+
+	var animaChange = function(event) {
+		changeDescription('anima', el.anima.select.val());
+	};
+
+	var changeDescription = function(anima, selected) {
+		var consumable = tswcalc.data.consumables[anima][selected];
+		if(consumable !== undefined) {
+			el[anima].description.html(consumable.description);
+		}
 	};
 
 	var oPublic = {
