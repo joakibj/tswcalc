4783f3afb6913513b4ffaa17e5c26af2ae7f89b5
joakibj <joakibj@gmail.com>
2013-11-03 17:48:14 +0100

    Updated export tests
    Separated creating bbcode export to its own test, since export.js does
    testing on the unbuilt source files

---
+++ test/export-integration.js
@@ -0,0 +1,19 @@
+module('export-integration-tests', {
+    setup: function() {
+        renderSlots();
+        renderButtonbar();
+        initiateSelectHandlers();
+        initiateButtonHandlers();
+        initiateRaidCheckboxes();
+        initiateSummary();
+        tswcalc.export.init();
+    }
+});
+
+test('should create BBCode export', 1, function() {
+    createTankBuild();
+
+    tswcalc.export.startExportBBCode();
+
+    equal($('#export-textarea').html(), bbcode);
+});
